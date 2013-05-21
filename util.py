306dc0d7e96d91b417a702230a9d34fa1dbcc289
Joe Cross <joe.mcross@gmail.com>
2013-05-20 18:37:41 -0700

    Add helpers for nested attributes
    getattrpath is for nested retrieval (getting obj.a.b.c with
    getattrpath(obj, 'a.b.c))
    
    prefix_keys generates key, value pairs with the prefix prepended to each
    key.

--- util.py
+++ util.py
@@ -12,5 +12,20 @@ def flatten(l, ltypes=collections.Sequence):
         if l:
             yield l.pop(0)
 
+
+def getattrpath(obj, path):
+    '''
+    path is a dot-delimited chain of attributes to look up.
+    getattrpath(my_object, 'a.b.c') returns my_object.a.b.c
+    '''
+    for attr in path.split('.'):
+        obj = getattr(obj, attr)
+    return obj
+
+
+def prefix_keys(prefix, dict):
+    for key, value in dict.items():
+        yield prefix + key, value
+
 noop = lambda self, *a, **kw: None
 passthrough = lambda v: v
