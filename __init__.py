f1156284efbb442905804acf16ec44fbf44a721b
Joe Cross <joe.mcross@gmail.com>
2013-05-21 20:46:07 -0700

    Added converters, dropped metadata class
    I cleaned up some of the indexes[] code so that there's only two points
    that touches the nested dictionaries (aside from register):
    - metadata_field, which is basic but obscures the somewhat confusing
    multi-nested dictionaries
    - has_registered_class, which takes a class, or a string representing a
    class, or a normalized string representing a class, and determines if
    that value is known to the serializer.
    
    Converters:
    - Intercept the values just before serialization/ just after
    deserialization so that they can be mapped to different types or
    coerced.  This is useful for, say, a value which is a bool.  It should
    be an int when serializing, and it's an int when it's deserialized, but
    we want to interact with it like a real bool.
    - Converters are given as a dictionary of string => [func, func] where
    both functions take a single value and return a single value.  The first
    function is called on the value stored in the object just before it's
    serialized.  For the bool example, this can be the built-in int
    function, which will convert a bool to an integer for serialization.
    The second function is called on the value that is deserialized from the
    bitstream.  For the bool example, this can be the built-in bool
    function, which will convert an int to a bool.  If either function is
    provided, the other must be as well.
    - Converters can be defined for specific attributes using
    attr_converters, where each key is the variable to apply conversions to.
    - Converters can also be defined for a format string, such that any
    attribute with that format string will run through those conversion
    functions.
    - Both attribute and format converters can be specified.  If any
    attribute is listed in the attribute converters and its format is in
    format converters, the attribute converter will be used.

--- __init__.py
+++ __init__.py
@@ -6,50 +6,102 @@ PREFIX = "Serial_"
 indexes = {}
 attributes = ['cls', 'cls_str', 'normalized_cls_str', 'raw_format', 'compact_format']
 
-#  Build indexes into SerializationMetadata
+#  Build indexes into metadata so we can get at it by most properties
 for attr in attributes:
     indexes[attr] = {}
 
 
-def register(cls, serial_format):
-    metadata = SerializationMetadata(cls, serial_format)
+def register(cls, format, attr_converters=None, fmt_converters=None):
+    metadata = generate_metadata(
+        cls, format,
+        attr_converters=attr_converters,
+        fmt_converters=fmt_converters
+    )
     for attr, index in indexes.items():
-        index[getattr(metadata, attr)] = metadata
+        index[metadata[attr]] = metadata
 
 
-class SerializationMetadata:
-    def __init__(self, cls, raw_format):
-        self.cls = cls
-        self.cls_str = cls.__name__
-        self.normalized_cls_str = PREFIX + self.cls_str
+def normalized_class_str(cls_or_str):
+    '''Centralized normalizing logic so we don't have PREFIX sprinkled throughout our functions'''
+    if not isinstance(cls_or_str, str):
+        cls_or_str = cls_or_str.__name__
+    return PREFIX + cls_or_str
 
-        self.raw_format = raw_format
-        pieces = self.raw_format.split(',')
+
+def generate_metadata(cls, raw_format, attr_converters=None, fmt_converters=None):
+        metadata = {}
+        metadata['cls'] = cls
+        metadata['cls_str'] = cls.__name__
+        metadata['normalized_cls_str'] = normalized_class_str(cls)
+        metadata['raw_format'] = raw_format
+
+        pieces = raw_format.split(',')
         compact_pieces = []
         for piece in pieces:
             name, format = [p.strip() for p in piece.split('=')]
-            if format in indexes['cls_str']:
-                compact_pieces.append('{}={}'.format(PREFIX+format, name))
+            if has_registered_class(format, normalized=False):
+                compact_pieces.append('{}={}'.format(normalized_class_str(format), name))
             else:
                 compact_pieces.append('{}={}'.format(format, name))
         compact_format = ','.join(compact_pieces)
-        self.compact_format = compact_format
+        metadata['compact_format'] = compact_format
 
-        formats = self.raw_format.split(',')
+        formats = raw_format.split(',')
         attrs = [fmt.split('=')[0].strip() for fmt in formats]
-        self.attrs = attrs
+        metadata['attrs'] = attrs
+
+        metadata['attr_converters'] = attr_converters or {}
+        metadata['fmt_converters'] = fmt_converters or {}
+
+        return metadata
+
+
+def metadata_field(index_type, index_value, metadata_field):
+    '''
+    index_type: string of the type of value you're finding metadata by.  Examples include 'cls' or 'raw_format'.
+    index_value: value of the previous type.  For 'cls' a class object, for 'raw_format' a format string.
+    metadata_field: string of the field type to retrieve.  This can be any of the values for index_type.
+    '''
+    return indexes[index_type][index_value][metadata_field]
+
+
+def has_registered_class(cls_or_str, normalized=False):
+    '''
+    Takes either a class object or the string of a class name.
+    normalized: True if the cls string is normalized.
+        class: <class 'MySerializableClass'>
+        class string: 'MySerializableClass'
+        normalized class string: 'Serial_MySerializableClass' (or whatever PREFIX is)
+    '''
+
+    if not normalized:
+        normalized_name = normalized_class_str(cls_or_str)
+    else:
+        normalized_name = cls_or_str
+    normalized_cls_dicts = indexes['normalized_cls_str']
+    return normalized_name in normalized_cls_dicts
 
 
 def serialize(obj):
     stream = bitstring.BitStream()
     cls = obj.__class__
-    cls_format = indexes['cls'][cls].compact_format
+
+    cls_format = metadata_field('cls', cls, 'compact_format')
+    attr_converters = metadata_field('cls', cls, 'attr_converters')
+    fmt_converters = metadata_field('cls', cls, 'fmt_converters')
+
     for argstr in cls_format.split(','):
         format, name = argstr.split('=')
         data = getattr(obj, name)
-        if format in indexes['normalized_cls_str']:
+        if has_registered_class(format, normalized=True):
             substream = serialize(data)
         else:
+            # Check for converters for this object.
+            # Attribute converters take precedence.
+            if name in attr_converters:
+                data = attr_converters[name][0](data)
+            elif format in fmt_converters:
+                data = fmt_converters[format][0](data)
             substream = bitstring.pack(format, data)
         stream.append(substream)
     return stream
@@ -60,25 +112,41 @@ def deserialize(cls, data, seek=True):
     if seek:
         data.pos = 0
 
-    cls_format = indexes['cls'][cls].compact_format
+    cls_format = metadata_field('cls', cls, 'compact_format')
+    attr_converters = metadata_field('cls', cls, 'attr_converters')
+    fmt_converters = metadata_field('cls', cls, 'fmt_converters')
+
     for argstr in cls_format.split(','):
         format, name = argstr.split('=')
-        if PREFIX in format:
-            subcls = indexes['normalized_cls_str'][format].cls
+        if has_registered_class(format, normalized=True):
+            subcls = metadata_field('normalized_cls_str', format, 'cls')
             kwargs[name] = deserialize(subcls, data, seek=False)
         else:
-            kwargs[name] = data.read(format)
+            value = data.read(format)
+            # Check for converters for this object.
+            # Attribute converters take precedence.
+            if name in attr_converters:
+                value = attr_converters[name][1](value)
+            elif format in fmt_converters:
+                value = fmt_converters[format][1](value)
+            kwargs[name] = value
     return cls.deserialize(**kwargs)
 
 
 def autoserialized(cls):
     '''The class must support an empty init function.'''
-    register(cls, cls.serial_format)
+    attr_converters = getattr(cls, 'serial_attr_converters', None)
+    fmt_converters = getattr(cls, 'serial_fmt_converters', None)
+    register(
+        cls, cls.serial_format,
+        attr_converters=attr_converters,
+        fmt_converters=fmt_converters
+    )
 
     @classmethod
     def deserialize(cls, **kwargs):
         obj = cls()
-        for attr in indexes['cls'][cls].attrs:
+        for attr in metadata_field('cls', cls, 'attrs'):
             setattr(obj, attr, kwargs.get(attr, None))
         return obj
     cls.deserialize = deserialize
@@ -89,12 +157,11 @@ def serial_dict(obj, prefix='', dict=None):
     if dict is None:
         dict = collections.OrderedDict()
 
-    fmt = indexes['cls'][obj.__class__].compact_format
+    fmt = metadata_field('cls', obj.__class__, 'compact_format')
     for argstr in fmt.split(','):
         format, attr_name = argstr.split('=')
         data = getattr(obj, attr_name)
-        if format in indexes['normalized_cls_str']:
-            # Format is registered as serializable, so data is an object to serialize
+        if has_registered_class(format, normalized=True):
             data_prefix = prefix + attr_name + '.'
             serial_dict(data, data_prefix, dict)
         else:
