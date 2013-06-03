3dcdb41b538fc43c138e9349a9ea4de611d04a4c
Joe <joe.mcross@gmail.com>
2013-06-02 22:15:57 -0700

    Version bump for README, crease changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Origami',
-    version='0.0.3',
+    version='0.0.4',
     author='Joe Cross',
     author_email='joe.mcross@gmail.com',
     packages=['origami'],
