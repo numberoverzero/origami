04b4b5ae7abb511703f24af403f0db5b65e72600
Joe Cross <joe.mcross@gmail.com>
2014-07-19 08:21:52 -0700

    set up tox

--- setup.py
+++ setup.py
@@ -1,15 +1,30 @@
-from distutils.core import setup
+from setuptools import setup
+import io
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+long_description = read('README.rst')
 
 setup(
     name='Origami',
-    version='0.1.1',
+    version='0.1.2',
     author='Joe Cross',
     author_email='joe.mcross@gmail.com',
     packages=['origami'],
     url='https://github.com/numberoverzero/origami',
     license='LICENSE.txt',
     description='Lightweight bit packing for classes',
-    long_description=open('README.rst').read(),
-    install_requires=["bitstring"],
-    requires=["bitstring"],
+    long_description=long_description,
+    install_requires=["bitstring"]
 )
---
+++ tasks.py
@@ -0,0 +1,26 @@
+import invoke
+
+
+def run(*args, **kwargs):
+    kwargs.update(echo=True)
+    return invoke.run(*args, **kwargs)
+
+
+@invoke.task
+def clean():
+    run("rm -rf dist/")
+    run("rm -rf origami.egg-info/")
+    run("find . -name '*.pyc' -delete")
+    run("find . -name '__pycache__' -delete")
+    run("rm -f .coverage")
+
+
+@invoke.task(clean)
+def build():
+    run("python setup.py develop")
+    pass
+
+
+@invoke.task(build)
+def test():
+    run('tox')
